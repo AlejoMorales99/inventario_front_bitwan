8a759dbb66f6934713651d0a8e85d1e962a0f711
AlejoMorales99 <leagueofleguends122@gmail.com>
2024-05-22 15:21:17 -0500

    cambios url

--- dotenv.ts
+++ dotenv.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.50.46:4001',
+  apiUrl: 'http://192.168.50.46:4001/api',
   ip_serber_pruebas_https: "https://pruebas.bitwan.info/api",
   url_login_produccion: "https://www.bitwan.info/api/public/login",
   portServerPruebas: "44080",
