5a39a406486acea1e5590db42cd46be4b9cce3b4
AlejoMorales99 <leagueofleguends122@gmail.com>
2024-05-22 09:06:49 -0500

    cambio de la url .env pruebas https

--- dotenv.ts
+++ dotenv.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://192.168.50.46:4001',
-  ip_serber_pruebas_https: "https://pruebas.bitawan.info/api",
+  ip_serber_pruebas_https: "https://pruebas.bitwan.info/api",
   url_login_produccion: "https://www.bitwan.info/api/public/login",
   portServerPruebas: "44080",
   password: "C0n3x1on4dm1n"
